825818a02f64c5c882fa8c0bac9fa9d7be9816d5
Valera <Valera@valera.com>
2023-08-27 21:39:36 +0300

    add service plan serializer

--- app/services/serializers.py
+++ app/services/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 
-from services.models import Subscription
+from services.models import Subscription, Plan
+
+
+class PlansSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Plan
+        fields = ('__all__')
 
 
 class SubscriptionsSerializer(serializers.ModelSerializer):
+    plan = PlansSerializer()
     client_name = serializers.CharField(source='client.company_name', read_only=True)
     email = serializers.CharField(source='client.user.email', read_only=True)
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan')
--- app/services/views.py
+++ app/services/views.py
@@ -12,6 +12,7 @@ class SubscriptionsViewSet(ReadOnlyModelViewSet):
     A simple ViewSet for viewing accounts.
     """
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
                                                               'user__email')),
     )
