ab8d63d1c5904a61cfa67cc502d1d85040a8bb20
Valera <Valera@valera.com>
2023-09-06 14:26:07 +0300

    Added an aggregation field to view for the discount in the total cost of service

--- app/services/serializers.py
+++ app/services/serializers.py
@@ -7,14 +7,18 @@ class PlansSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Plan
-        fields = ('__all__')
+        fields = '__all__'
 
 
 class SubscriptionsSerializer(serializers.ModelSerializer):
     plan = PlansSerializer()
     client_name = serializers.CharField(source='client.company_name', read_only=True)
     email = serializers.CharField(source='client.user.email', read_only=True)
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return instance.price
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email', 'plan')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
--- app/services/views.py
+++ app/services/views.py
@@ -1,5 +1,4 @@
-from django.db.models import Prefetch
-from django.shortcuts import render
+from django.db.models import Prefetch, F
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -14,6 +13,7 @@ class SubscriptionsViewSet(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
-                                                              'user__email')),
-    )
+                                                                                     'user__email')),
+    ).annotate(price=F('service__full_price') -
+                     F('service__full_price') * F('plan__discount_percent') / 100.00)
     serializer_class = SubscriptionsSerializer
