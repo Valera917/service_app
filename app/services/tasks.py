14121cb9c792e250bc8c34ad8f521245eb2d02e0
Valera Medovkin <v.medovkin1664@gmail.com>
2023-09-13 09:57:43 +0300

    implement celery_singleton

--- app/services/tasks.py
+++ app/services/tasks.py
@@ -1,8 +1,9 @@
 from celery import shared_task
+from celery_singleton import Singleton
 from django.db.models import F
 
 
-@shared_task
+@shared_task(base=Singleton)
 def set_price(subscription_id):
     from services.models import Subscription
 
